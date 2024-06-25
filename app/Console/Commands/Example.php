a163ffc041f5c682ca7777e0f465e0dc54cbc79d
Fabio Vige <fabiovige@gmail.com>
2024-06-25 20:40:16 -0300

    configuração de horizon e supervisor

---
+++ app/Console/Commands/Example.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Example extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mla';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        dispatch(new \App\Jobs\MlaExample());
+        return Command::SUCCESS;
+    }
+}
